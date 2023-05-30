7901b8b124ab89215d67c4df9b683fe820e717e2
Draegonis <ddmDraegonis@hotmail.com>
2023-05-30 10:56:47 -0400

    Added TintState to state enums.

--- src/enums/state.ts
+++ src/enums/state.ts
@@ -16,3 +16,6 @@ export type DdmGameState = LitteralUnion<(typeof GameState)[number]>;
 
 export const WeatherState = ["none", "rain", "storm", "snow"] as const;
 export type DdmWeatherState = LitteralUnion<(typeof WeatherState)[number]>;
+
+export const TintState = ["normal", "dawn", "dusk", "cloudy", "night"] as const;
+export type DdmTintState = LitteralUnion<(typeof TintState)[number]>;
