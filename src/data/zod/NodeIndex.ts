a35399bc024c286e5e99f79da58ada27a0b77cc7
Draegonis <ddmDraegonis@hotmail.com>
2023-05-30 12:06:14 -0400

    Added comment to the calenderSchema.

--- src/data/zod/NodeIndex.ts
+++ src/data/zod/NodeIndex.ts
@@ -1,6 +1,9 @@
 import { number as zNum, object as zObj, infer as zInfer } from "zod";
 import { zStringArray } from "./zodIndex";
 
+/**
+ * The zod schema to parse a calender from rmmz params.
+ */
 export const calenderSchema = zObj({
   months: zStringArray,
   weeksInMonth: zNum(),
