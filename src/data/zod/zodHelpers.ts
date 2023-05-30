f723e65a79a3cb4c65fc77363b65b9d45aed2fae
Draegonis <ddmDraegonis@hotmail.com>
2023-05-30 12:21:09 -0400

    Added JsDoc to zodHelpers.ts.

--- src/data/zod/zodHelpers.ts
+++ src/data/zod/zodHelpers.ts
@@ -2,6 +2,12 @@ import { string as zString } from "zod";
 import type { ZodSchema } from "zod";
 import type { DdmParserFuncs } from "../../types/ddmTypes";
 
+/**
+ * An async function to fetch data from a json file and parse it with a zod schema.
+ * @param {string} path - the string path to the json file.
+ * @param {ZodSchema} schema - the schema used to parse the json file that is read.
+ * @returns { Promise<T> } - a Promise of type <T> given to the function.
+ */
 export const fetchJsonSchema = async <T>(path: string, schema: ZodSchema) => {
   const fetchJson = async () => {
     return await fetch(path, {
@@ -22,7 +28,16 @@ export const fetchJsonSchema = async <T>(path: string, schema: ZodSchema) => {
   }
   return returnData as Promise<T>;
 };
-
+/**
+ * A function that uses z.string() and transforms on it to be able to parse a string
+ * without having to use fetch to get json data.
+ * @param {string} target - the string to be converted into an object.
+ * @param {DdmParserFuncs} parserFuncs - the object containing the functions needed to
+ * parse each key.
+ * @param {boolean | undefined} isArray - a boolean that allows to parse the string as an
+ * array of the same object. Example DdmNodeEvent[].
+ * @returns { {[key:string]: unknown} | { [key: string]: unknown }[] }
+ */
 export const parseStructSchema = (
   target: string,
   parserFuncs: DdmParserFuncs,
@@ -30,28 +45,28 @@ export const parseStructSchema = (
 ): { [key: string]: unknown } | { [key: string]: unknown }[] => {
   return (
     zString()
-    .transform((value) => JSON.parse(value))
+      .transform((value) => JSON.parse(value))
       // Note: Add error handling.
-    .transform((value) => {
-      const parser = (targetValue: string) =>
-        Object.fromEntries(
-          Object.entries(targetValue).map(([key, value]) => {
-            if (parserFuncs[key]) {
-              return [key, parserFuncs[key](value)];
-            }
-            return [key, value];
-          })
-        );
+      .transform((value) => {
+        const parser = (targetValue: string) =>
+          Object.fromEntries(
+            Object.entries(targetValue).map(([key, value]) => {
+              if (parserFuncs[key]) {
+                return [key, parserFuncs[key](value)];
+              }
+              return [key, value];
+            })
+          );
 
-      if (isArray) {
-        return (value as []).map((entry) => {
-          const newEntry = JSON.parse(entry);
-          return parser(newEntry);
-        });
-      }
+        if (isArray) {
+          return (value as []).map((entry) => {
+            const newEntry = JSON.parse(entry);
+            return parser(newEntry);
+          });
+        }
 
-      return parser(value);
-    })
+        return parser(value);
+      })
       .parse(target)
   );
 };
